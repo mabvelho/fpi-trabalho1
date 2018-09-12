1643daef59ffc80412a97100f12d3be671e87138
mabvelho <mabvelho@gmail.com>
2018-09-12 12:56:37 -0300

    Add files via upload

--- src/main/ImageApp.java
+++ src/main/ImageApp.java
@@ -4,6 +4,7 @@ import java.awt.FlowLayout;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -36,7 +37,7 @@ public class ImageApp extends JApplet {
 		return image;
 	}
 
-	private void createFrame(ImageIcon icon, JFrame frame) {
+	private static void frameCreate(ImageIcon icon, JFrame frame) {
 		frame.setLayout(new FlowLayout());
 		frame.setSize(icon.getIconWidth() + 25, icon.getIconHeight() + 45);
 
@@ -48,39 +49,67 @@ public class ImageApp extends JApplet {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	private void updateFrame(ImageIcon icon, JFrame frameTargetImg) {
+	private static void frameUpdate(ImageIcon icon, JFrame frame) {
 		// TODO Auto-generated method stub
+		frame.removeAll();
+		frameCreate(icon, frame);
+	}
+
+	
+	private BufferedImage imgFlipHorizontal(BufferedImage image) {
+		int imgWidth = image.getWidth();
+		int imgHeight = image.getHeight();
+		int imgType = image.getType();
 		
-	}
-
-	
-	private BufferedImage imgFlipHorizontal(BufferedImage imgSource) {
-	    BufferedImage imageTarget = new BufferedImage(imgSource.getWidth(), imgSource.getHeight(), BufferedImage.TYPE_INT_ARGB);
-	    Graphics2D graphics = imageTarget.createGraphics();
+	    BufferedImage newImage = new BufferedImage(imgWidth, imgHeight, imgType);
+	    Graphics2D graphics = newImage.createGraphics();
 	    
-	    graphics.drawImage(imgSource, imgSource.getHeight(), 0, -imgSource.getWidth(), imgSource.getHeight(), null);
+	    
+	    //graphics.drawImage(image, 0, 0, imgWidth, imgHeight, imgWidth, 0, 0, imgHeight, null);
+	    graphics.drawImage(image, image.getWidth(), 0, -image.getWidth(), image.getHeight(), null);
 	    graphics.dispose();
 	    
-	    return imageTarget;
+	    return newImage;
 	}
 	
-	private BufferedImage imgFlipVertical(BufferedImage imgSource) {
-	    BufferedImage imageTarget = new BufferedImage(imgSource.getWidth(), imgSource.getHeight(), BufferedImage.TYPE_INT_ARGB);
-	    Graphics2D graphics = imageTarget.createGraphics();
+	private BufferedImage imgFlipVertical(BufferedImage image) {
+		
+		BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D graphics = newImage.createGraphics();
 	    
-	    graphics.drawImage(imgSource, imgSource.getHeight(), 0, -imgSource.getWidth(), imgSource.getHeight(), null);
+	    graphics.drawImage(image, 0, image.getHeight(), image.getWidth(), -image.getHeight(), null);
 	    graphics.dispose();
 	    
-	    return imageTarget;
+	    return newImage;
 	}
 	
-
-	private void getImgPixels(BufferedImage imgSource, int[][] imgSourcePixels) {
-		for (int i = 0; i < imgSource.getWidth(); i++) {
-			for (int j = 0; j < imgSource.getHeight(); j++) {
-				imgSourcePixels[i][j] = imgSource.getRGB(i, j);
+	public static BufferedImage imgRotate(BufferedImage image, int angle) {  
+        int imgWidth = image.getWidth();  
+        int imgHeight = image.getHeight();  
+        int imgType = image.getType();
+        
+        BufferedImage newImage = new BufferedImage(imgWidth, imgHeight, imgType);  
+        Graphics2D graphics = newImage.createGraphics();  
+        graphics.rotate(Math.toRadians(angle), imgWidth/2, imgHeight/2);  
+        graphics.drawImage(image, null, 0, 0);  
+        return newImage;  
+    } 		
+	
+	private void imgExtractPixels(int[][] imgPixels, BufferedImage image) {
+		for (int i = 0; i < image.getWidth(); i++) {
+			for (int j = 0; j < image.getHeight(); j++) {
+				imgPixels[i][j] = image.getRGB(i, j);
 			}
 		}
+	}
+	
+	private BufferedImage imgReplacePixels(BufferedImage image, int[][] imgPixels) {
+		for (int i = 0; i < image.getWidth(); i++) {
+			for (int j = 0; j < image.getHeight(); j++) {
+				image.setRGB(i, j, imgPixels[i][j]);
+			}
+		}
+		return image;
 	}
 	
 	public static void main(String[] args) {
@@ -95,9 +124,9 @@ public class ImageApp extends JApplet {
 		ImageIcon iconSource = new ImageIcon(imgSource);
 		ImageIcon iconTarget = new ImageIcon(imgTarget);
 
-		JFrame frameSourceImg = new JFrame();
-		JFrame frameTargetImg = new JFrame();
-
+		JFrame frameImgSource = new JFrame("Source Image");
+		JFrame frameImgTarget = new JFrame("Target Image");
+		
 		//int[][] imgSourcePixels = new int[imgWidth][imgHeight];
 
 		//imgApp.getImgPixels(imgSource, imgSourcePixels);
@@ -105,14 +134,22 @@ public class ImageApp extends JApplet {
 		//int[][] imgTargetPixels = imgSourcePixels.clone();
 		
 		
-		imgApp.createFrame(iconSource, frameSourceImg);
-		imgApp.createFrame(iconTarget, frameTargetImg);
+		ImageApp.frameCreate(iconSource, frameImgSource);
+		ImageApp.frameCreate(iconTarget, frameImgTarget);
+		//white 16777215
 		
+		try {
+			File outputFile = new File ("mini2.jpg");
+			ImageIO.write(imgTarget, "PNG", outputFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		imgTarget = imgApp.imgFlipHorizontal(imgTarget);
-		imgTarget = imgApp.imgFlipHorizontal(imgTarget);
-		
-		imgApp.createFrame(iconTarget, frameTargetImg);
+		System.out.println(imgTarget.toString());
+		imgTarget = imgApp.imgFlipVertical(imgSource);
+		System.out.println(imgTarget.toString());
+		iconTarget = null;
+		iconTarget = new ImageIcon(imgTarget);
 	}
 
 
